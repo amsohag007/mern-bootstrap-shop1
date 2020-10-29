10408b042ecd8d44e6688f8c08cdc685054f2b60
amsohag007 <amsohag007@gmail.com>
2020-10-29 21:26:46 +0600

    backend api setup, dev environment setup, fetching data from server to clientside(axos), envronment variable setup

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
-import products from "../products";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <h1>products</h1>
+      <h1>Latest products</h1>
       <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3}>
