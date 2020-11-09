d83a333fe3c1732f233a9ff262541b0103329007
amsohag007 <amsohag007@gmail.com>
2020-11-10 00:05:06 +0600

    pagination

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -21,11 +21,15 @@ import {
 } from "../constants/productConstansts.js";
 
 //actions for products on HomeScreen
-export const listProducts = (keyword = "") => async (dispatch) => {
+export const listProducts = (keyword = "", pageNumber = "") => async (
+  dispatch
+) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
-    const { data } = await axios.get(`/api/products?keyword=${keyword}`);
+    const { data } = await axios.get(
+      `/api/products?keyword=${keyword}&pageNumber=${pageNumber}`
+    );
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
